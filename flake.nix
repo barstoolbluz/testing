e4bf56444686da9f6d7664905f3d49f14e68cc6b
sswoyer <54217314+barstoolbluz@users.noreply.github.com>
2024-07-23 22:10:31 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs";
   };
 
-  outputs = { self, nixpkgs, ... }:
+  outputs = { self, nixpkgs }:
     let
       supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
@@ -43,42 +43,9 @@
             '';
           };
         in
-        nodejs
-      );
-
-      defaultPackage = forAllSystems (system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-          nodejsFor = {
-            x86_64-linux = {
-              url = "https://nodejs.org/dist/v22.5.1/node-v22.5.1-linux-x64.tar.gz";
-              sha256 = "sha256-KnuLiqXHOa5VIz1Z94c2kRqKXaXqHGPw6EPaJw0DlJk=";
-            };
-            x86_64-darwin = {
-              url = "https://nodejs.org/dist/v22.5.1/node-v22.5.1-darwin-x64.tar.gz";
-              sha256 = "sha256-astFM7wKQ6Ro+Qu9SSMKoWx8V7KjRR7+Ahdf7qNGdU0=";
-            };
-            aarch64-darwin = {
-              url = "https://nodejs.org/dist/v22.5.1/node-v22.5.1-darwin-arm64.tar.gz";
-              sha256 = "sha256-dgI4SFXx4Wm2DlHDYOWixnK4mhnM2gGZzkZ11o/vqvI=";
-            };
-            aarch64-linux = {
-              url = "https://nodejs.org/dist/v22.5.1/node-v22.5.1-linux-arm64.tar.gz";
-              sha256 = "sha256-jfr0ss48Y5dx9r5wAbrIHs4O6jzWZoZmwAEAvxdaFO4=";
-            };
-          };
-          nodejs = pkgs.stdenv.mkDerivation {
-            pname = "nodejs";
-            version = "22.5.1";
-            src = pkgs.fetchurl (nodejsFor.${system});
-            buildInputs = [ pkgs.stdenv ];
-            installPhase = ''
-              mkdir -p $out
-              cp -r * $out/
-            '';
-          };
-        in
-        nodejs
+        {
+          default = nodejs;
+        }
       );
     };
 }
