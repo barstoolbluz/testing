8c3e4e04ae49a57f55df5e6e4f10b55b2d57949c
daedalus <daedalus@localhost.localdomain>
2024-07-23 21:20:39 -0400

    new flake

--- flake.nix
+++ flake.nix
@@ -1,40 +1,31 @@
 {
-  description = "Description for the project";
+  description = "Flake for Node.js v22.5.1";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs";
   };
 
-  outputs = inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [ ];
-      systems = [ "x86_64-linux" ];
-      perSystem = { self, pkgs, ... }: {
-        packages = {
-          default = pkgs.stdenv.mkDerivation {
-            name = "nodejs22.5.1";
-            src = pkgs.fetchurl {
-              url = "https://nodejs.org/dist/v22.5.1/node-v22.5.1-linux-x64.tar.gz";
-              sha256 = "sha256-KnuLiqXHOa5VIz1Z94c2kRqKXaXqHGPw6EPaJw0DlJk=";
-            };
-            installPhase = ''
-              echo "installing nodejs"
-              mkdir -p $out
-              cp -r ./ $out/
-            '';
+  outputs = { self, nixpkgs }: {
+    packages = {
+      x86_64-linux = let
+        pkgs = import nixpkgs { system = "x86_64-linux"; };
+        nodejs = pkgs.stdenv.mkDerivation {
+          pname = "nodejs";
+          version = "22.5.1";
+          src = pkgs.fetchurl {
+            url = "https://nodejs.org/dist/v22.5.1/node-v22.5.1-linux-x64.tar.gz";
+            sha256 = "sha256-KnuLiqXHOa5VIz1Z94c2kRqKXaXqHGPw6EPaJw0DlJk";
           };
+          buildInputs = [ pkgs.stdenv ];
+          installPhase = ''
+            mkdir -p $out
+            cp -r * $out/
+          '';
         };
-
-        devShells.default = pkgs.mkShell {
-          buildInputs = [
-            pkgs.gnused
-            pkgs.yarn
-            self.packages.default
-            pkgs.nodePackages.node-gyp-build
-            pkgs.nodePackages.kafkajs
-          ];
-        };
+      in
+      { 
+        default = nodejs;
       };
     };
+  };
 }
