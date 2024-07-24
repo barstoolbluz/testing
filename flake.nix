529b26274d6f5deada7e9ea801edc48780c9d179
sswoyer <54217314+barstoolbluz@users.noreply.github.com>
2024-07-24 16:16:07 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,41 +1,31 @@
 {
-  description = "VSCode for specific platforms";
+  description = "VSCode with a very specific set of extensions";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
-    packages = {
-      x86_64-linux = let
-        pkgs = import nixpkgs {
-          config.allowUnfree = true;
-          system = "x86_64-linux";
-        };
-      in
-      {
-        default = pkgs.vscode;
-      };
-
-      x86_64-darwin = let
-        pkgs = import nixpkgs {
-          config.allowUnfree = true;
-          system = "x86_64-darwin";
-        };
-      in
-      {
-        default = pkgs.vscode;
-      };
-
-      aarch64-darwin = let
-        pkgs = import nixpkgs {
-          config.allowUnfree = true;
-          system = "aarch64-darwin";
-        };
-      in
-      {
-        default = pkgs.vscode;
-      };
+  outputs =
+    { self, nixpkgs }:
+    {
+      packages = builtins.mapAttrs (
+        system: pkgs:
+        let
+          pkgs = import nixpkgs {
+            config.allowUnfree = true;
+            inherit system;
+          };
+        in
+        rec {
+          default = vscode-with-extensions;
+          vscode-with-extensions = pkgs.vscode-with-extensions.override {
+            vscodeExtensions = with pkgs.vscode-extensions; [
+              bbenoist.nix
+              ms-python.python
+              ms-azuretools.vscode-docker
+            ];
+          };
+        }
+      ) nixpkgs.legacyPackages;
     };
-  };
 }
