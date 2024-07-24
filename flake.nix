604558d46372f4fa48d4499d9a0ce5515f1d7bf3
sswoyer <54217314+barstoolbluz@users.noreply.github.com>
2024-07-24 14:55:59 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Flake for Node.js v22.5.1";
+  description = "Flake for Node.js v20.13.1";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
@@ -16,25 +16,25 @@
           pkgs = import nixpkgs { inherit system; };
           nodejsFor = {
             x86_64-linux = {
-              url = "https://nodejs.org/dist/v22.5.1/node-v22.5.1-linux-x64.tar.gz";
-              sha256 = "sha256-KnuLiqXHOa5VIz1Z94c2kRqKXaXqHGPw6EPaJw0DlJk=";
+              url = "https://nodejs.org/dist/v20.13.1/node-v20.13.1-linux-x64.tar.xz";
+              sha256 = "sha256-78Dyld2HjlEKsS6ja7rcPbA8aHqzDAfobHzbp+7Yeak=";
             };
             x86_64-darwin = {
-              url = "https://nodejs.org/dist/v22.5.1/node-v22.5.1-darwin-x64.tar.gz";
-              sha256 = "sha256-astFM7wKQ6Ro+Qu9SSMKoWx8V7KjRR7+Ahdf7qNGdU0=";
+              url = "https://nodejs.org/dist/v20.13.1/node-v20.13.1-darwin-x64.tar.gz";
+              sha256 = "sha256-gL3pXcl2uE21ylZnOMBzBQh65Xj187BRkeDm/1Sq6vI=";
             };
             aarch64-darwin = {
-              url = "https://nodejs.org/dist/v22.5.1/node-v22.5.1-darwin-arm64.tar.gz";
-              sha256 = "sha256-dgI4SFXx4Wm2DlHDYOWixnK4mhnM2gGZzkZ11o/vqvI=";
+              url = "https://nodejs.org/dist/v20.13.1/node-v20.13.1-darwin-arm64.tar.gz";
+              sha256 = "sha256-ww/llfWdzSxRWNpr+LwlH/yFyjcwSvqJ2xUPs8YsRQc=";
             };
             aarch64-linux = {
-              url = "https://nodejs.org/dist/v22.5.1/node-v22.5.1-linux-arm64.tar.gz";
-              sha256 = "sha256-jfr0ss48Y5dx9r5wAbrIHs4O6jzWZoZmwAEAvxdaFO4=";
+              url = "https://nodejs.org/dist/v20.13.1/node-v20.13.1-linux-arm64.tar.xz";
+              sha256 = "sha256-T5wv+xFoVdb6S2ZU5FP0A+MbUyhLgceJtz0dLiDG9xA=";
             };
           };
           nodejs = pkgs.stdenv.mkDerivation {
             pname = "nodejs";
-            version = "22.5.1";
+            version = "20.13.1";
             src = pkgs.fetchurl (nodejsFor.${system});
             buildInputs = [ pkgs.stdenv ];
             installPhase = ''
