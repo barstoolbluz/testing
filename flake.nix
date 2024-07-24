58e9d5c2aae5cf361296a600d55a9c20c87d535c
sswoyer <54217314+barstoolbluz@users.noreply.github.com>
2024-07-23 22:05:31 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,40 +1,51 @@
 {
-  description = "Flake for Node.js v22.5.1 with support for x86_64-linux and x86_64-darwin";
+  description = "Flake for Node.js v22.5.1";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
   };
-  outputs = { self, nixpkgs }: {
-    packages.x86_64-linux.default =
-      let
-        pkgs = import nixpkgs { system = "x86_64-linux"; };
-      in
-      pkgs.stdenv.mkDerivation {
-        pname = "nodejs";
-        version = "22.5.1";
-        src = pkgs.fetchurl {
-          url = "https://nodejs.org/dist/v22.5.1/node-v22.5.1-linux-x64.tar.gz";
-          sha256 = "sha256-KnuLiqXHOa5VIz1Z94c2kRqKXaXqHGPw6EPaJw0DlJk";
-        };
-        installPhase = ''
-          mkdir -p $out
-          tar -xzf $src -C $out --strip-components=1
-        '';
-      };
-    packages.x86_64-darwin.default =
-      let
-        pkgs = import nixpkgs { system = "x86_64-darwin"; };
-      in
-      pkgs.stdenv.mkDerivation {
-        pname = "nodejs";
-        version = "22.5.1";
-        src = pkgs.fetchurl {
-          url = "https://nodejs.org/dist/v22.5.1/node-v22.5.1-darwin-x64.tar.gz";
-          sha256 = "sha256-astFM7wKQ6Ro+Qu9SSMKoWx8V7KjRR7+Ahdf7qNGdU0=";
-        };
-        installPhase = ''
-          mkdir -p $out
-          tar -xzf $src -C $out --strip-components=1
-        '';
-      };
-  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          nodejsFor = {
+            x86_64-linux = {
+              url = "https://nodejs.org/dist/v22.5.1/node-v22.5.1-linux-x64.tar.gz";
+              sha256 = "sha256-KnuLiqXHOa5VIz1Z94c2kRqKXaXqHGPw6EPaJw0DlJk=";
+            };
+            x86_64-darwin = {
+              url = "https://nodejs.org/dist/v22.5.1/node-v22.5.1-darwin-x64.tar.gz";
+              sha256 = "sha256-astFM7wKQ6Ro+Qu9SSMKoWx8V7KjRR7+Ahdf7qNGdU0=";
+            };
+            aarch64-darwin = {
+              url = "https://nodejs.org/dist/v22.5.1/node-v22.5.1-darwin-arm64.tar.gz";
+              sha256 = "sha256-dgI4SFXx4Wm2DlHDYOWixnK4mhnM2gGZzkZ11o/vqvI=";
+            };
+            aarch64-linux = {
+              url = "https://nodejs.org/dist/v22.5.1/node-v22.5.1-linux-arm64.tar.gz";
+              sha256 = "sha256-jfr0ss48Y5dx9r5wAbrIHs4O6jzWZoZmwAEAvxdaFO4=";
+            };
+          };
+          nodejs = pkgs.stdenv.mkDerivation {
+            pname = "nodejs";
+            version = "22.5.1";
+            src = pkgs.fetchurl (nodejsFor.${system});
+            buildInputs = [ pkgs.stdenv ];
+            installPhase = ''
+              mkdir -p $out
+              cp -r * $out/
+            '';
+          };
+        in
+        {
+          default = nodejs;
+        }
+      );
+    };
 }
